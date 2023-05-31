6001367147a6a3943c03ac8fd8e1ac7bf7ab50bc
Benjamin Mora T <114621426+profe-benja@users.noreply.github.com>
2023-05-31 17:29:05 -0400

    Initial commit

---
+++ sucursal/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+# Create your models here.
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f"{self.nombre}"
+
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=100)
+    correo = models.EmailField(max_length=100, unique=True)
+    clave = models.CharField(max_length=256)
+    
+    def __str__(self):
+        return f"{self.nombre} {self.correo}"
+
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcio = models.CharField(max_length=256)
+    codigo = models.CharField(max_length=100, unique=True)
+    precio = models.IntegerField()
+    foto = models.FileField(upload_to="uploads/", null=True, blank=True)
+    id_usuario = models.ForeignKey('Usuario', on_delete=models.CASCADE)
+    id_categoria = models.ForeignKey('Categoria', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.nombre}"
+
+
+
+    
+    
